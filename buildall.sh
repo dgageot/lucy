655c7ed14ad0ae499aa7adcb6bbaced775303983
David Gageot <david@gageot.net>
2014-10-14 12:44:06 +0200

    Improvements

--- buildall.sh
+++ buildall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-LUCY="$(pwd)/$(dirname $0)"
-
-cd $LUCY/git && docker build -t dgageot/git .
-cd $LUCY/gcloud && docker build -t dgageot/gcloud .
-cd $LUCY/kube && docker build -t dgageot/kube .
-cd $LUCY/jug && docker build -t dgageot/jug .
-cd $LUCY/eclipse && docker build -t dgageot/eclipse .
+docker build -t dgageot/git git
+docker build -t dgageot/eclipse eclipse
+docker build -t dgageot/jug jug
+docker build -t dgageot/gcloud gcloud
+docker build -t dgageot/kube kube
---
+++ prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+unregister-java.sh
+brew unlink git
+brew unlink maven
